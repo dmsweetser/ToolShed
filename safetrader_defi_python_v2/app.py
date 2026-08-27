384a2156b8d7d98aaaedd152a0101df73120cab8
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-27 12:06:48 -0400

    Port fix

--- safetrader_defi_python_v2/app.py
+++ safetrader_defi_python_v2/app.py
@@ -218,4 +218,4 @@ def handle_clear_patterns():
     emit_state()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5050, debug=True)
