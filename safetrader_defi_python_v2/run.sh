84c4897e626170a8a6e61ebd652b5c3addc5adfb
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-28 12:32:10 -0400

    Get rid of python version

--- safetrader_defi_python_v2/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Activating virtual environment..."
-source venv/bin/activate
-export FLASK_APP=app.py
-export FLASK_ENV=development
-echo "Starting Flask application..."
-python app.py
