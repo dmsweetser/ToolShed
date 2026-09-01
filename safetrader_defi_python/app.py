8d3f02416d99d0cf0efc12c042c4c1de026f5d24
Daniel Sweetser <dmsweetser@gmail.com>
2026-08-31 21:39:08 -0400

    Latest python defi

--- safetrader_defi_python/app.py
+++ safetrader_defi_python/app.py
@@ -19,10 +19,10 @@ load_dotenv()
 
 # Configure logging with enhanced verbosity
 logging.basicConfig(
-    level=logging.DEBUG,  # Set to DEBUG for maximum verbosity
+    level=logging.INFO,  # Set to DEBUG for maximum verbosity
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     handlers=[
-        logging.FileHandler("app_debug.log"),
+        logging.FileHandler("app.log"),
         logging.StreamHandler(),
     ],
 )
@@ -217,10 +217,42 @@ UNISWAP_V3_FACTORY_ABI = json.loads('''
 
 UNISWAP_V3_POOL_ABI = json.loads('''
 [
-    {"inputs": [], "name": "token0", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "stateMutability": "view", "type": "function"},
-    {"inputs": [], "name": "token1", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "stateMutability": "view", "type": "function"},
-    {"inputs": [], "name": "fee", "outputs": [{"internalType": "uint24", "name": "", "type": "uint24"}], "stateMutability": "view", "type": "function"},
-    {"inputs": [], "name": "slot0", "outputs": [{"internalType": "uint160", "name": "sqrtPriceX96"}, {"internalType": "int24", "name": "tick"}, {"internalType": "uint16", "name": "observationIndex"}, {"internalType": "uint16", "name": "observationCardinality"}, {"internalType": "uint16", "name": "observationCardinalityNext"}, {"internalType": "uint8", "name": "feeProtocol"}, {"internalType": "bool", "name": "unlocked"}], "stateMutability": "view", "type": "function"}
+    {
+        "inputs": [],
+        "name": "slot0",
+        "outputs": [
+            {"internalType": "uint160", "name": "sqrtPriceX96", "type": "uint160"},
+            {"internalType": "int24", "name": "tick", "type": "int24"},
+            {"internalType": "uint16", "name": "observationIndex", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinality", "type": "uint16"},
+            {"internalType": "uint16", "name": "observationCardinalityNext", "type": "uint16"},
+            {"internalType": "uint8", "name": "feeProtocol", "type": "uint8"},
+            {"internalType": "bool", "name": "unlocked", "type": "bool"}
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token0",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "token1",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "fee",
+        "outputs": [{"internalType": "uint24", "name": "", "type": "uint24"}],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ]
 ''')
 
